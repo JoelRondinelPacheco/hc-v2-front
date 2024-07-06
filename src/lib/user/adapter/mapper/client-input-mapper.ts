5e1126617a9d1526611b4a84c9b08c89cdf85b04
JoelRondinelPacheco <joel.rondinel.pacheco@gmail.com>
2024-07-06 20:26:54 -0300

    Refactor users module

---
+++ src/lib/user/adapter/mapper/client-input-mapper.ts
@@ -0,0 +1,41 @@
+import { InputMapper } from "@/lib/common/adapter/mapper/mapper"
+import { ClientDTO } from "../../application/dto/client-dto"
+import { CreateClientRequest, UpdateClientRequest } from "../../domain/client.entity"
+
+export const createClientInputMapper = (): InputMapper<ClientDTO, CreateClientRequest, UpdateClientRequest> => {
+    return {
+        driverDTOtoSave: clientDTOToSave,
+        driverDTOtoUpdate: clientDTOToUpdate
+    }
+}
+export const clientDTOToSave = (dto: ClientDTO): CreateClientRequest => {
+    const { name, lastname, email, address, dni, birthday, phoneNumber, roleId, password } = dto;
+    return {
+        name,
+        lastname,
+        email,
+        address,
+        dni,
+        birthday,
+        phoneNumber,
+        roleId,
+        password,
+    }
+}
+
+export const clientDTOToUpdate = (dto: ClientDTO): UpdateClientRequest => {
+    const { id, personId, name, lastname, email, address, dni, birthday, phoneNumber, roleId, password } = dto;
+    return {
+        id: id,
+        personId,
+        name,
+        lastname,
+        email,
+        address,
+        dni,
+        birthday,
+        phoneNumber,
+        roleId,
+        password,
+    }
+}
---
+++ src/lib/user/adapter/mapper/employee-output-mapper.ts
@@ -0,0 +1,46 @@
+import { OutputMapper } from "@/lib/common/adapter/mapper/mapper";
+import { CreateEmployeeRequest, EmployeeEntity, UpdateEmployeeRequest } from "../../domain/employee.entity";
+
+const createEmployeeOutputMapper = (): OutputMapper<EmployeeEntity, CreateEmployeeRequest, UpdateEmployeeRequest> => {
+    return {
+        saveToEntity,
+        updateToEntity,
+    }
+}
+
+const saveToEntity = (dto: CreateEmployeeRequest, id: number): EmployeeEntity => {
+    const { name, lastname, email, address, dni, birthday, phoneNumber, roleId, password, salary } = dto;
+    return {
+        id: id,
+        salary,
+        person: {
+            id: id,
+            name,
+            lastname,
+            email,
+            address,
+            dni,
+            birthday,
+            phoneNumber,
+        }
+    }
+}
+
+
+const updateToEntity = (dto: UpdateEmployeeRequest): EmployeeEntity => {
+    const { name, lastname, email, address, dni, birthday, phoneNumber, roleId, password , salary, id, personId} = dto;
+    return {
+        id: id,
+        salary,
+        person: {
+            id: personId,
+            name,
+            lastname,
+            email,
+            address,
+            dni,
+            birthday,
+            phoneNumber,
+        }
+    }
+}
